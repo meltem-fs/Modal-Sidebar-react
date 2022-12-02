955458349d3caefc038682d4ad4700a27f22be3f
MELTEM <meltemfsanaldi@gmail.com>
2022-12-02 21:13:12 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Modal from "./Modal";
 import Sidebar from "./Sidebar";
 import Home from "./Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Team from "./pages/Team";
+import Projects from "./pages/Projects";
+import Documents from "./pages/Documents";
+import Calendar from "./pages/Calendar";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          
           <Route path="/" element={<Home />}>
             <Route path="sidebar" element={<Sidebar />} />
-            
+            <Route path="team" element={<Team/>}/>
+            <Route path="projects" element={<Projects/>}/>
+            <Route path="documents" element={<Documents/>}/>
+            <Route path="calendar" element={<Calendar/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
